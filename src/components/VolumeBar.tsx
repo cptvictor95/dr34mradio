d2bec809dbae55e1958e2fe7aa443520bc5e6e66
Sean Uchida <seankuchida@gmail.com>
2022-12-18 15:05:25 -0300

    volume bar getting player context

--- src/components/VolumeBar.tsx
+++ src/components/VolumeBar.tsx
@@ -1,13 +1,24 @@
-import { useContext, BaseSyntheticEvent } from "react";
+import { useState, useContext, BaseSyntheticEvent, useEffect } from "react";
 import PlayerContext from "../contexts/PlayerContext";
 
 export const VolumeBar = () => {
     const player = useContext(PlayerContext);
+    const [volume, setVolume] = useState(0);
+
     function handleVolume(e: BaseSyntheticEvent) {
       if (player.videoPlayer != null) {
         player.changeVolume(e.target.value);
+        setVolume(e.target.value);
       }
     }
+
+    // make an useEffect to get the current volume
+    // and set it to the input value
+    useEffect(() => {
+        if (player.videoPlayer.current) {
+            setVolume(player.videoPlayer.getVolume);
+        }
+    }, []);
   
     return (
       <div>
@@ -16,6 +27,7 @@ export const VolumeBar = () => {
           type="range"
           min="0"
           max="100"
+          value={volume}
           className="range"
         />
       </div>
