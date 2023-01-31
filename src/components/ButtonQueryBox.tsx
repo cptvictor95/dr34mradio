d72894f656b65bf286eae6089db66b584e6ac8c4
cptvictor95 <cpt.victor@hotmail.com>
2023-01-30 23:37:02 -0300

    refact: split button query box and query type selector to components

---
+++ src/components/ButtonQueryBox.tsx
@@ -0,0 +1,22 @@
+export const ButtonQueryBox: React.FC<{
+  handleSubmit: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}> = ({ handleSubmit }) => {
+  return (
+    <button onClick={(e) => handleSubmit(e)} className="btn btn-square">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </button>
+  );
+};
--- src/components/ytQueryBox.tsx
+++ src/components/ytQueryBox.tsx
@@ -4,6 +4,8 @@ import type { YTQueryType } from "../types/YTQueryType";
 import type { ResultItem, YTResult } from "../types/YTResult";
 import { trpc } from "../utils/trpc";
 import { debounce } from "lodash";
+import { QueryTypeSelector } from "./QueryTypeSelector";
+import { ButtonQueryBox } from "./ButtonQueryBox";
 
 const YTQueryBox: React.FC = () => {
   const [keywordString, setKeywordString] = useState("");
@@ -84,54 +86,6 @@ const InputBox: React.FC<{
         )}
       </div>
     </div>
-  );
-};
-
-const QueryTypeSelector: React.FC<{
-  ytQueryTypeRef: React.MutableRefObject<YTQueryType>;
-}> = ({ ytQueryTypeRef }) => {
-  const [ytQueryType, setYTQueryType] = useState<YTQueryType>("TEXT");
-
-  function handleSelectQuery(e: React.ChangeEvent<HTMLSelectElement>) {
-    if (e.target.value === "Texto" && ytQueryType === "LINK") {
-      setYTQueryType("TEXT");
-      ytQueryTypeRef.current = "TEXT";
-    } else {
-      setYTQueryType("LINK");
-      ytQueryTypeRef.current = "LINK";
-    }
-  }
-
-  return (
-    <select
-      onChange={(e) => handleSelectQuery(e)}
-      className="select-bordered select max-w-[100px] text-white"
-    >
-      <option disabled>Tipo de busca</option>
-      <option defaultValue="true">Texto</option>
-      <option>Link</option>
-    </select>
-  );
-};
-
-const ButtonQueryBox = ({ handleSubmit }) => {
-  return (
-    <button onClick={(e) => handleSubmit(e)} className="btn btn-square">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-        />
-      </svg>
-    </button>
   );
 };
 
